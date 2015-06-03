aa0022301991fb631493e84a9df5447d3fe81ae4
lnmunhoz <ln.munhoz@gmail.com>
2015-06-03 01:26:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# chrome-omnibox-kickass
+Kickass search made easy
+
+[MIT License](http://lnmunhoz.mit-license.org/) © Lucas N. Munhoz
---
+++ background.js
@@ -0,0 +1,16 @@
+(function(){
+  'use strict';
+
+  chrome.omnibox.setDefaultSuggestion({
+    description: '%s Search on Kickass Torrents'
+  });
+
+  chrome.omnibox.onInputEntered.addListener(function(text) {
+    var url = "https://kickass.to/search/" + text;
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.update(tabs[0].id, {url: url});
+    });
+  });
+
+})();
